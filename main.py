4a406f4dc0ae9f2922f45b9a373cc345b1d36586
Peter Manda <109303643+PeterManda19@users.noreply.github.com>
2022-10-22 16:41:14 +0000

    Personalised cooking recipe

---
+++ main.py
@@ -0,0 +1,7 @@
+food = input("A type of food: ")
+plant = input("A type of plant: ")
+cooking = input("A method of cooking: ")
+burned = input("A word to describe burned food: ")
+item = input("A household item: ")
+print()
+print(cooking, food, "with", burned, plant, "on a bed of", item)
