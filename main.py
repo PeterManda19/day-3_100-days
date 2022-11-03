2d1b6e4da6ffb168be8bac3017c285f190cdc098
Peter Manda <109303643+PeterManda19@users.noreply.github.com>
2022-11-03 17:01:47 +0000

    defensive coding

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+import time
+
 food = input("A type of food: ")
 plant = input("A type of plant: ")
 cooking = input("A method of cooking: ")
 burned = input("A word to describe burned food: ")
 item = input("A household item: ")
 print()
-print(cooking, food, "with", burned, plant, "on a bed of", item)
+
+print(cooking.upper(), food, "with", burned, plant, "on a bed of", item)
+
+
+def endGame():
+  while True:
+    print()
+    x= input("""Thank you for reading!
+To read again please click Stop on top right page and click Run """)
+    print()
+    continue
+
+
+if __name__ == "__main__":
+  time.sleep(2)
+  endGame()
